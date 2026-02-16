4a686390a2acf93d77c2102f930f5fd6fef27b74
ChipironWeaver <lowen.tisserand@gmail.com>
2026-02-16 18:58:36 +0100

    [Fix] Collider not rotating + rotate the player not just the sprite

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,33 +5,33 @@ using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 1;
-    [SerializeField] private Transform _spriteObject;
-
     private Vector2 _move;
     private Rigidbody2D _rb;
     private float _spriteRotation = 0f;
+    private Transform _transform;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _transform = GetComponent<Transform>();
     }
     void Update()
     {
         if (_move != Vector2.zero)
         {
-            _rb.linearVelocity = _speed * _move;
+            
             _spriteRotation = Vector2.Angle(Vector2.up, _move);
             if (_move.x > 0)
             {
                 _spriteRotation = 180-_spriteRotation + 180;
             }
+            _rb.linearVelocity = _speed * _move;
         }
         else
         {
             _rb.linearVelocity = Vector3.zero;
-            _spriteRotation = 0;
         }
-        _spriteObject.localRotation = Quaternion.Euler(0, 0 , _spriteRotation);
+        _transform.localRotation = Quaternion.Euler(0, 0 , _spriteRotation);
     }
     void OnMove(InputValue value)
     {
