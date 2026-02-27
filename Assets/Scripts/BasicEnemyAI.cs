1b6c81d2ac5b7c0b1094b7cd27aa002478d7e3e3
ChipironWeaver <lowen.tisserand@gmail.com>
2026-02-27 23:08:54 +0100

    [Minor] Sprite for enemies

--- Assets/Scripts/BasicEnemyAI.cs
+++ Assets/Scripts/BasicEnemyAI.cs
@@ -5,12 +5,17 @@ public class BasicEnemyAI : MonoBehaviour
 {
     [SerializeField] private Transform _target;
     [SerializeField] private float _moveSpeed = 1f;
-    
+    [SerializeField] private bool _rotationBasedMovement;
+
     private Transform _transform;
+    private Rigidbody2D _rigidbody2D;
+    private float _spriteRotation;
+    private Vector2 _playerDirection;
 
     private void Start()
     {
         _transform = GetComponent<Transform>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
         if (_target == null)
         {
             _target = PlayerController.Instance.transform;
@@ -19,6 +24,17 @@ public class BasicEnemyAI : MonoBehaviour
 
     void FixedUpdate()
     {
-        _transform.position = Vector3.MoveTowards(_transform.position, _target.position, _moveSpeed * Time.deltaTime);
+        _playerDirection =  _target.position - _transform.position;
+        _playerDirection.Normalize();
+        if (_rotationBasedMovement) 
+        {
+            _spriteRotation = Vector2.Angle(Vector2.up, _playerDirection);
+            if (_playerDirection.x > 0)
+            {
+                _spriteRotation = 180 - _spriteRotation + 180;
+            }
+            _transform.localRotation = Quaternion.Euler(0, 0 , _spriteRotation);
+        }
+        _rigidbody2D.linearVelocity = _moveSpeed * _playerDirection;
     }
 }
